189754fc77fe496d7e2231a187cdc6b30ef45a3b
Haeyoon-Cho <yoonn_ee@naver.com>
2022-07-06 02:40:22 +0900

    hh

---
+++ static/js/post_detail.js
@@ -0,0 +1,19 @@
+const checkbox = document.querySelector(".checkbox");
+console.log(checkbox);
+
+checkbox.addEventListener("click", click);
+
+function click() {
+  if (document.body.classList.contains("dark")) {
+    document.body.classList.remove("dark");
+    console.log("convert into Light Mode");
+  } else {
+    document.body.classList.add("dark");
+    console.log("convert into Dark Mode");
+  }
+}
+
+function toggleClick() {
+  document.body.classList.toggle("dark");
+  console.log("Working!");
+}
